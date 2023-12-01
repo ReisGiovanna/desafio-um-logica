5a8966f959ff32fc146cb8721d4b83a5bc120df7
ReisGiovanna <giovannareisdev@gmail.com>
2023-12-01 19:56:25 -0300

    Adicionando estrutura de decisão

--- desafio1.js
+++ desafio1.js
@@ -1,9 +1,21 @@
 let nomeHeroi = "Fulano"
 let xpHeroi = 0
+let nivelHeroi = ""
 const introducao = "Informações do herói: "
 
 for (let i = 0; i < 1002; i++)
 {
     xpHeroi += 1
-    console.log(xpHeroi)
 }
+
+switch (xpHeroi)
+{
+    case (xpHeroi < 1000):
+        nivelHeroi = "Ferro"
+
+    default:
+        nivelHeroi = "Ainda está crescendo!"
+}
+
+console.log(introducao + "\nO herói de nome " + nomeHeroi +
+" está no nível " + nivelHeroi)

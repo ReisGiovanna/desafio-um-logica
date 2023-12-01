8697730204e7aa904041ee183d3e5b1a080945c3
ReisGiovanna <giovannareisdev@gmail.com>
2023-12-01 19:47:46 -0300

    Adicionando o laço de repetição

--- desafio1.js
+++ desafio1.js
@@ -1,1 +1,9 @@
-console.log("hello word")
+let nomeHeroi = "Fulano"
+let xpHeroi = 0
+const introducao = "Informações do herói: "
+
+for (let i = 0; i < 1002; i++)
+{
+    xpHeroi += 1
+    console.log(xpHeroi)
+}
